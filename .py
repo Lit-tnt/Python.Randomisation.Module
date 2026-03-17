824383d587f42c19145113f202517c841ea19632
Lit-tnt <fdyuile@outlook.com>
2026-03-17 08:18:46 +0000

    Save current project changes

---
+++ .py
@@ -0,0 +1,16 @@
+=====================================================
+    PYTHON RANDOMI + {Z, S} + ATION THE SECOND ;)    
+=====================================================
+1. Generate a random number
+2. Flip a coin
+3. Roll a die
+4. Pick a random name
+5. Random time (12 HOUR)
+6. Random time (24 HOUR)
+7. Pick a random float
+8. Random playing card
+9. Random list of names
+10. Generate a humungous number in various bases
+11. Generate a random string of gibberish
+Input in the intiger of your choice
+1
